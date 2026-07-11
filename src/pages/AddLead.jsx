a4e903b877fb5289748e052289d37cd6c561dab2
supportzrto <support.zrto@gmail.com>
2026-07-11 16:45:31 +0530

    relsoved edit lead page issues

--- src/pages/AddLead.jsx
+++ src/pages/AddLead.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { addLead } from "../services/api";
+
 import { useNavigate } from "react-router-dom";
 // import { getInventory } from "../services/api";
 import { addLead, getInventory, getSalesUsers } from "../services/api";
